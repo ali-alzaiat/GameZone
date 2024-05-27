1f00e2995d339bca9ec98655df00bc2c57de3503
ali-alzaiat <alialzaiat2000@gmail.com>
2024-05-27 23:52:41 +0300

    Created Game Model

---
+++ GameZone/GameZone/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+        [MaxLength(500)]
+        public string Cover { get; set; } = string.Empty;
+        public int CategorId { get; set; }
+        public Category Category { get; set; } = default!;
+
+        public ICollection<GameDevice> Device { get; set;} = new List<GameDevice>();
+
+    }
+}
