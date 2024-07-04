cb886b6463cc3855746106acbaf80e38fae788dc
ali-alzaiat <alialzaiat2000@gmail.com>
2024-07-04 15:15:44 +0300

    Created a View Model for create game form.

---
+++ GameZone/GameZone/ViewModels/CreateGameFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
+{
+    public class CreateGameFormViewModel
+    {
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+        public List<int> SelectedDevices { get; set; } = new List<int>();
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+        public IFormFile Cover { get; set; } = default;
+    }
+}
