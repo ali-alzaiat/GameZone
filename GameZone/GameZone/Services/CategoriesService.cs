6074f2591bb8c2c6175c5ae86dcd1cb3255517cb
ali-alzaiat <alialzaiat2000@gmail.com>
2024-07-05 22:12:29 +0300

    Created categories service.

---
+++ GameZone/GameZone/Services/CategoriesService.cs
@@ -0,0 +1,17 @@
+﻿using GameZone.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Services
+{
+    public class CategoriesService(ApplicationDBContext context) : ICategoriesService
+    {
+        readonly private ApplicationDBContext _context = context;
+        public IEnumerable<SelectListItem> GetSelectList()
+        {
+            return _context.Categories.Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                .OrderBy(comparer => comparer.Text)
+                .ToList();
+        }
+    }
+}
